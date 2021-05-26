758655f647bffaa9e7e8655cfcbae94b202c53d9
Raoul Shah <74040700+Raoul-Shah@users.noreply.github.com>
2021-05-26 21:23:50 +0200

    Added Images

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ let navigation = document.querySelector('.navigation');
 menutoggle.onclick = function() {
     menutoggle.classList.toggle('active');
     navigation.classList.toggle('active');
-}
+};
+
+let imgBx = document.querySelectorAll('.imgBx');
+imgBx.forEach(popup => popup.addEventListener('click', () => {
+    popup.classList.toggle('active');
+}));
